3bb3e7852b53deaee98a681642d08aa6c8be7cf8
Leslie Marxen <leslie.marxen98@gmail.com>
2020-12-01 17:15:04 +0100

    extracted io to own file

--- day1/solution.pl
+++ day1/solution.pl
@@ -1,23 +1,6 @@
 :- use_module(library(clpfd)).
+:- include("../io_util.pl").
 
-read_input_lines_codes(_, Acc, Acc).
-read_input_lines_codes(Stream, Out) :- 
-    read_line_to_string(Stream, Line),
-    Line \= end_of_file,
-    Line \= "",
-    read_input_lines_codes(Stream, OutR),
-    Out = [Line | OutR], !.
-
-read_input_lines_codes(Stream, Out) :- 
-    read_input_lines_codes(Stream, Out, []).
-
-get_puzzle_inputs(Inputs) :- get_puzzle_inputs("input.txt", Inputs).
-get_puzzle_inputs(Filepath, Inputs):- 
-    open(Filepath, read, File),
-    read_input_lines_codes(File, Inputs),
-    close(File).
-
-% ACTUAL PUZZLE
 
 contains(E, [E|_]).
 contains(C, [_|Es]) :- contains(C, Es).
