24693a4300ab2c443ecd4e07adcf4aa874c49eaa
Leslie Marxen <leslie.marxen98@gmail.com>
2020-12-02 08:06:18 +0100

    day2 fix datatype conversions

--- day2/solution.pl
+++ day2/solution.pl
@@ -11,14 +11,15 @@ extract_values_from_definition(Definition, Min, Max, Char, Password) :-
     re_split('-', Definition, Split1),
     Split1 = [Minc, _, No_Min],
     re_split(' ', No_Min, Split2),
-    Split2 = [Maxc, _, Char_Weird, _, Password],
+    Split2 = [Maxc, _, Char_Weird, _, Password_Str],
+    string_chars(Password_Str, Password),
     number_string(Min, Minc),
     number_string(Max, Maxc),
     string_chars(Char_Weird, [Char, ':']).
 
 
 valid_password(Input):-
-    extract_values_from_definition(Input, Xc, Yc, C, Password),
+    extract_values_from_definition(Input, X, Y, C, Password),
     counter(Password, C, Occurances),
     Occurances >= X,
     Occurances =< Y.
