ff5c6142a2ee55634ad250b67837507a7ed4f5e6
Leslie Marxen <leslie.marxen98@gmail.com>
2020-12-03 16:42:34 +0100

    day3 param now tuple

--- day3/solution.pl
+++ day3/solution.pl
@@ -14,10 +14,10 @@ remove_n(N, [_|Ins], Out) :-
     remove_n(New_N, Ins, Out).
 
 
-gen_path(Right, Down, Inputs, Path) :- gen_path(Right, Down, 0, Inputs, Path).
+gen_path(Slope, Inputs, Path) :- gen_path(Slope, 0, Inputs, Path).
 
-gen_path(_Right, _Down, _Depth, [], []).
-gen_path(Right, Down, Depth, [Line|Inputs], Path) :- 
+gen_path(_Slope, _Depth, [], []).
+gen_path([Right, Down], Depth, [Line|Inputs], Path) :- 
     string_chars(Line, Chars),
     length(Chars, Len),
     Modulo is (Depth * Right) mod Len,
@@ -27,20 +27,21 @@ gen_path(Right, Down, Depth, [Line|Inputs], Path) :-
     Skip_Down is Down - 1,
     remove_n(Skip_Down, Inputs, Tail),
     
-    gen_path(Right, Down, New_Depth, Tail, Result),
+    gen_path([Right, Down], New_Depth, Tail, Result),
     Path = [Value|Result], !.
 
 
 solution1(Inputs, Result) :- 
-    gen_path(3, 1, 0, Inputs, Path),
+    gen_path([3, 1], 0, Inputs, Path),
     counter(Path, #, Result).
     
 solution2(Inputs, Result) :-
-    gen_path(1, 1, Inputs, Path1),
-    gen_path(3, 1, Inputs, Path2),
-    gen_path(5, 1, Inputs, Path3),
-    gen_path(7, 1, Inputs, Path4),
-    gen_path(1, 2, Inputs, Path5),
+    Slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]],
+    gen_path([1, 1], Inputs, Path1),
+    gen_path([3, 1], Inputs, Path2),
+    gen_path([5, 1], Inputs, Path3),
+    gen_path([7, 1], Inputs, Path4),
+    gen_path([1, 2], Inputs, Path5),
     counter(Path1, #, Trees1),
     counter(Path2, #, Trees2),
     counter(Path3, #, Trees3),
