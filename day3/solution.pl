110c7da22b46727d80239e9c431a95ba0d000315
Leslie Marxen <leslie.marxen98@gmail.com>
2020-12-03 16:54:42 +0100

    day3 refactor

--- day3/solution.pl
+++ day3/solution.pl
@@ -1,11 +1,11 @@
 :- include("../io_util.pl").
 :- use_module(library(clpfd)).
 
-counter([], _, 0).
-counter([E|Es], E, Count) :-
-    counter(Es, E, Acc), !,
+counter(_, [], 0).
+counter(E, [E|Es],  Count) :-
+    counter(E, Es, Acc), !,
     Count is Acc + 1.
-counter([_|Es], E, Count) :- counter(Es, E, Count).
+counter(E, [_|Es], Count) :- counter(E, Es, Count).
 
 remove_n(0, In, In) :- !.
 remove_n(_, [], []) :- !.
@@ -13,8 +13,9 @@ remove_n(N, [_|Ins], Out) :-
     New_N is N - 1,
     remove_n(New_N, Ins, Out).
 
+mul(V1, V2, R) :- R is V1*V2.
 
-gen_path(Slope, Inputs, Path) :- gen_path(Slope, 0, Inputs, Path).
+gen_path(Inputs, Slope, Path) :- gen_path(Slope, 0, Inputs, Path).
 
 gen_path(_Slope, _Depth, [], []).
 gen_path([Right, Down], Depth, [Line|Inputs], Path) :- 
@@ -32,27 +33,18 @@ gen_path([Right, Down], Depth, [Line|Inputs], Path) :-
 
 
 solution1(Inputs, Result) :- 
-    gen_path([3, 1], 0, Inputs, Path),
-    counter(Path, #, Result).
+    gen_path(Inputs, [3,1], Path),
+    counter(#, Path, Result).
     
 solution2(Inputs, Result) :-
     Slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]],
-    gen_path([1, 1], Inputs, Path1),
-    gen_path([3, 1], Inputs, Path2),
-    gen_path([5, 1], Inputs, Path3),
-    gen_path([7, 1], Inputs, Path4),
-    gen_path([1, 2], Inputs, Path5),
-    counter(Path1, #, Trees1),
-    counter(Path2, #, Trees2),
-    counter(Path3, #, Trees3),
-    counter(Path4, #, Trees4),
-    counter(Path5, #, Trees5),
-    Result is Trees1 * Trees2 * Trees3 * Trees4 * Trees5.
-
+    maplist(gen_path(Inputs), Slopes, Paths),
+    maplist(counter(#), Paths, Tree_Counts),
+    foldl(mul, Tree_Counts, 1, Result).
     
 main :-
     get_puzzle_inputs(Inputs),
     solution1(Inputs, Solution1),
-    solution2(Inputs, Solution2),
     write("Part1: "), writeln(Solution1),
+    solution2(Inputs, Solution2),
     write("Part2: "), writeln(Solution2).
