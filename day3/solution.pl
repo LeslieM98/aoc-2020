bc9b8e6b89d367109376812d422279a3911810f0
Leslie Marxen <leslie.marxen98@gmail.com>
2020-12-03 15:48:53 +0100

    day3 part2 removed param

--- day3/solution.pl
+++ day3/solution.pl
@@ -18,16 +18,16 @@ gen_path_d1(Right, Depth, [Line|Inputs], Path) :-
     gen_path_d1(Right, New_Depth, Inputs, Result),
     Path = [Value|Result], !.
 
-gen_path_d2(_Right, _Depth, [], []).
-gen_path_d2(_Right, _Depth, [_Ignored], []).
-gen_path_d2(Right, Depth, [Line, _Skipped|Inputs], Path) :- 
+gen_path_d2(_Depth, [], []).
+gen_path_d2(_Depth, [_Ignored], []).
+gen_path_d2(Depth, [Line, _Skipped|Inputs], Path) :- 
     string_chars(Line, Chars),
     length(Chars, Len),
-    Modulo is (Depth * Right) mod Len,
+    Modulo is Depth mod Len,
     nth0(Modulo, Chars, Value),
 
     New_Depth is Depth + 1,
-    gen_path_d2(Right, New_Depth, Inputs, Result),
+    gen_path_d2(New_Depth, Inputs, Result),
     Path = [Value|Result], !.
 
 solution1(Inputs, Result) :- 
@@ -39,7 +39,7 @@ solution2(Inputs, Result) :-
     gen_path_d1(3, 0, Inputs, Path2),
     gen_path_d1(5, 0, Inputs, Path3),
     gen_path_d1(7, 0, Inputs, Path4),
-    gen_path_d2(1, 0, Inputs, Path5),
+    gen_path_d2(0, Inputs, Path5),
     counter(Path1, #, Trees1),
     counter(Path2, #, Trees2),
     counter(Path3, #, Trees3),
@@ -49,7 +49,7 @@ solution2(Inputs, Result) :-
 
     
 main :-
-    get_puzzle_inputs("sample.txt", Inputs),
+    get_puzzle_inputs("istannen.txt", Inputs),
     solution1(Inputs, Solution1),
     solution2(Inputs, Solution2),
     write("Part1: "), writeln(Solution1),
