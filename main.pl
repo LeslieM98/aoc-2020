73dbafec2b859824b081a38a1e241bc5897c369e
Leslie Marxen <leslie.marxen98@gmail.com>
2020-12-04 05:01:47 +0100

    add reflective puzzlecalls

--- main.pl
+++ main.pl
@@ -3,19 +3,35 @@
 :- use_module("day02/day02", [main/3 as day02]).
 :- use_module("day03/day03", [main/3 as day03]).
 
+daynr_string(Day_Nr, Str) :- 
+    Day_Nr > 9,
+    number_string(Day_Nr, Str), !.
+daynr_string(Day_Nr, Str) :-
+    Day_Nr < 10,
+    number_string(Day_Nr, Day_Nr_Str),
+    string_chars(Day_Nr_Str, Day_Nr_Chars),
+    Char_Result = ['0'|Day_Nr_Chars],
+    string_chars(Str, Char_Result), !.
+
+day_predicate(Day, Predicate_Name) :-
+    daynr_string(Day, Day_Str),
+    atom_concat('day', Day_Str, Predicate_Name).
+
+day_path(Day, Path) :- 
+    daynr_string(Day, Day_Str),
+    string_concat("day", Day_Str, Dir),
+    string_concat(Dir, "/input.txt", Path).
+
+execute_puzzle(Day) :-
+    day_path(Day, Path),
+    get_puzzle_inputs(Path, D1_Input),
+    day_predicate(Day, Predicate),
+    call(Predicate, D1_Input, D1P1, D1P2),
+    % day01(D1_Input, D1P1, D1P2),
+    writeln(D1P1),
+    writeln(D1P2).
 
 main :-
-    get_puzzle_inputs("day01/input.txt", D1_Input),
-    day01(D1_Input, D1P1, D1P2),
-    writeln(D1P1),
-    writeln(D1P2),
-    
-    get_puzzle_inputs("day02/input.txt", D2_Input),
-    day02(D2_Input, D2P1, D2P2),
-    writeln(D2P1),
-    writeln(D2P2),
-    
-    get_puzzle_inputs("day03/input.txt", D3_Input),
-    day03(D3_Input, D3P1, D3P2),
-    writeln(D3P1),
-    writeln(D3P2).
+    execute_puzzle(1),
+    execute_puzzle(2),
+    execute_puzzle(3).
